eb16ab82211b6c420191a5eddb9d55568be8aa08
JennaLeysens <jennalicowley@gmail.com>
2020-08-24 17:40:07 +0200

    React router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/home" component={Home} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
