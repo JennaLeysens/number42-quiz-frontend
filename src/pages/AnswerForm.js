84a320126683f0b40f86b89b02bb38075b385754
JennaLeysens <jennalicowley@gmail.com>
2020-08-28 16:46:25 +0200

    Select input for round number

--- src/pages/AnswerForm.js
+++ src/pages/AnswerForm.js
@@ -7,6 +7,7 @@ import {
   FormLabel,
   Button,
   Box,
+  Select,
 } from "@chakra-ui/core";
 import "./AnswerForm.css";
 
@@ -15,6 +16,18 @@ export default function AnswerForm() {
     <Box>
       <Box className="container" spacing={3}>
         <Heading>Add your answers here</Heading>
+        <Stack>
+          <FormLabel>Round number</FormLabel>
+          <Select w={300}>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+          </Select>
+        </Stack>
         <Stack isInline>
           <Input w={700} size="lg"></Input>
           <FormLabel>Points</FormLabel>
