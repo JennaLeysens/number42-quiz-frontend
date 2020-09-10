c5e47e4a19d6581b1df4d7bbc488add8804d71f5
JennaLeysens <jennalicowley@gmail.com>
2020-09-10 16:54:02 +0200

    Added state to round form

--- src/pages/Round.js
+++ src/pages/Round.js
@@ -2,20 +2,27 @@ import React, { useState } from "react";
 import {
   Heading,
   Stack,
-  Input,
   NumberInput,
   FormLabel,
   Button,
   Box,
-  Select,
 } from "@chakra-ui/core";
 
 export default function Round() {
+  const [round, setRound] = useState();
+
   return (
     <Box>
       <Stack>
+        <Heading>Round number</Heading>
         <FormLabel>Round number</FormLabel>
-        <NumberInput w={300} min={1} max={10}></NumberInput>
+        <NumberInput
+          w={300}
+          min={1}
+          max={10}
+          value={round}
+          onChange={(e) => setRound(e.target.value)}
+        ></NumberInput>
       </Stack>
       <Button>Start</Button>
     </Box>
