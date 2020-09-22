517c6404652b8d6b1933923879e6541f27fe407b
JennaLeysens <jennalicowley@gmail.com>
2020-09-22 17:34:49 +0200

    State and variables updated on answerForm

--- src/pages/AnswerForm.js
+++ src/pages/AnswerForm.js
@@ -10,35 +10,45 @@ import {
   Box,
 } from "@chakra-ui/core";
 import "./AnswerForm.css";
-import { addAnswer } from "../store/User/actions";
+import { addAnswer, quizAdded } from "../store/User/actions";
 import { useParams } from "react-router-dom";
 
 export default function AnswerForm() {
   const dispatch = useDispatch();
   const { id } = useParams();
-  console.log(id);
-  const [qnumber1, setQNumber1] = useState();
-  const [qnumber2, setQNumber2] = useState();
-  const [qnumber3, setQNumber3] = useState();
-  const [qnumber4, setQNumber4] = useState();
-  const [qnumber5, setQNumber5] = useState();
-  const [qnumber6, setQNumber6] = useState();
-  const [qnumber7, setQNumber7] = useState();
-  const [qnumber8, setQNumber8] = useState();
-  const [qnumber9, setQNumber9] = useState();
-  const [qnumber10, setQNumber10] = useState();
+  const { round } = useParams();
+  console.log(id, round);
+  const [answer, setAnswer] = useState();
+  const [points, setPoints] = useState();
+
+  const quizId = id;
+  const roundId = round;
+
+  function submitForm() {
+    dispatch(addAnswer(quizId, roundId, answer, points));
+  }
 
   return (
     <Box>
       <Box className="container" spacing={3}>
         <Heading>Add your answers here</Heading>
         <Stack isInline>
-          <Input placeholder="Type your answer here" w={700} size="lg"></Input>
+          <Input
+            placeholder="Type your answer here"
+            w={700}
+            size="lg"
+            value={answer}
+            onChange={(e) => setAnswer(e.target.value)}
+          ></Input>
           <FormLabel>Points</FormLabel>
-          <NumberInput size="sm"></NumberInput>
+          <NumberInput
+            size="sm"
+            value={points}
+            onChange={(e) => setPoints(e.target.value)}
+          ></NumberInput>
         </Stack>
       </Box>
-      <Button m={10} size="md">
+      <Button m={10} size="md" onClick={submitForm}>
         Submit answers
       </Button>
     </Box>
