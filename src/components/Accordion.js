efad8ae3282de36ff9d8a77b80049f0098f586aa
JennaLeysens <jennalicowley@gmail.com>
2020-10-27 17:30:15 +0100

    Cleaned up Accordian code

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,22 +1,8 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
 import { Button, Box, Icon } from "@chakra-ui/core";
-import { selectUser } from "../store/User/selector";
-import Round from "./Round";
 
 export default function Accordion(props) {
   const [isShowing, setIsShowing] = useState(false);
-  const user = useSelector(selectUser);
-  // const rounds = user.quizDetails ? user.quizDetails.rounds : null;
-  // const numbers = rounds.map((round) => {
-  //   return round.roundNumber;
-  // });
-  // const answers = rounds
-  //   ? rounds.map((round) => {
-  //       return round.answers;
-  //     })
-  //   : null;
-  // console.log("answers", answers);
 
   return (
     <Box>
